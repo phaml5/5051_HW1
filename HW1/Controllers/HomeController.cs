d38363cfa680c5a5a624e498bb622e7630f25a52
Loi Pham <phaml5@seattleu.edu>
2019-05-17 04:26:14 -0700

    Hooked up the Model to Home and Index View

--- HW1/Controllers/HomeController.cs
+++ HW1/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace HW1.Controllers
                 LastAccessDate = DateTime.Now
             };
 
-            return View();
+            return View(myData);
         }
 
         public ActionResult FAQ()
